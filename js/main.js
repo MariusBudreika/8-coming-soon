6afe4cff94d452ab9d616cb595c47015fa7fd179
MariusBudreika <mariusbudreika09@gmail.com>
2021-04-19 16:53:55 +0300

    progress bar animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import { clock } from './components/clock.js';
+import { progressBar } from './components/progressBar.js';
+import { progressBarData } from './data/progressbarData.js';
+
 
 clock('#clock_birthday', '03-15')
+progressBar('.left-column', progressBarData);
